9ba8a85f7bf61fc947c529931b1a9b1ad5ce2e7d
jeff <jeffersondarcy@users.noreply.github.com>
2020-02-01 16:13:12 +0100

    fetch data

--- src/Posts/Posts.js
+++ src/Posts/Posts.js
@@ -1,6 +1,27 @@
 //@flow
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
 export default function Posts() {
-return <div>bla</div>
+    const [hasError, setErrors] = useState(false);
+    const [posts, setPosts] = useState({});
+
+    async function fetchData() {
+        const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+        res
+            .json()
+            .then(res => setPosts(res))
+            .catch(err => setErrors(err));
+    }
+
+    useEffect(() => {
+        fetchData();
+    });
+
+    return (
+        <div>
+            <span>{JSON.stringify(posts)}</span>
+            <hr />
+            <span>Has error: {JSON.stringify(hasError)}</span>
+        </div>
+    );
 }
